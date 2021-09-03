059666606174d7eaf5cc1305af71ba7f08311b48
yongjun <ruzun88@gmail.com>
2021-09-03 23:57:49 +0900

    svg 테스트

---
+++ habithub/src/views/GrassField.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <g transform="translate(10, 20)">
+        <g transform="translate(0, 0)">
+            <rect width="10" height="10" x="14" y="65" class="cc ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-01" data-level="0"></rect>
+            <rect width="10" height="10" x="14" y="78" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-02" data-level="0"></rect>
+        </g>
+        <g transform="translate(14, 0)">
+            <rect width="10" height="10" x="13" y="0" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-03" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="13" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-04" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="26" class="ContributionCalendar-day" rx="2" ry="2" data-count="18" data-date="2021-01-05" data-level="4"></rect>
+            <rect width="10" height="10" x="13" y="39" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-06" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="52" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-07" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="65" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-08" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="78" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-09" data-level="0"></rect>
+        </g>
+        <g transform="translate(28, 0)">
+            <rect width="10" height="10" x="13" y="0" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-03" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="13" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-04" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="26" class="ContributionCalendar-day" rx="2" ry="2" data-count="18" data-date="2021-01-05" data-level="4"></rect>
+            <rect width="10" height="10" x="13" y="39" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-06" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="52" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-07" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="65" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-08" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="78" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-09" data-level="0"></rect>
+        </g>
+        <g transform="translate(42, 0)">
+            <rect width="10" height="10" x="13" y="0" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-03" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="13" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-04" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="26" class="ContributionCalendar-day" rx="2" ry="2" data-count="18" data-date="2021-01-05" data-level="4"></rect>
+            <rect width="10" height="10" x="13" y="39" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-06" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="52" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-07" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="65" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-08" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="78" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-09" data-level="0"></rect>
+        </g>
+        <g transform="translate(56, 0)">
+            <rect width="10" height="10" x="13" y="0" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-03" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="13" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-04" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="26" class="ContributionCalendar-day" rx="2" ry="2" data-count="18" data-date="2021-01-05" data-level="4"></rect>
+            <rect width="10" height="10" x="13" y="39" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-06" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="52" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-07" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="65" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-08" data-level="0"></rect>
+            <rect width="10" height="10" x="13" y="78" class="ContributionCalendar-day" rx="2" ry="2" data-count="0" data-date="2021-01-09" data-level="0"></rect>
+        </g>
+        <text x="14" y="-7" class="ContributionCalendar-label">Jan</text>
+      </g>
+  </svg>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+svg {
+  width: 722px;
+  height: 112px;
+}
+rect {
+  transition: all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+rect:hover, rect:focus {
+  fill: gray;
+}
+</style>
